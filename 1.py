4efcf23e05458dc19be94b01d37e7a1ef9ac2314
MarsFengXinjie <fengxinjie96@gmail.com>
2019-03-06 16:59:30 +0800

    Two Sum

--- 1.py
+++ 1.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  6 16:04:18 2019
+
+@author: mars
+"""
+
+"""
+Given an array of integers, return indices of the two numbers such that they
+add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may
+not use the same element twice.
+"""
 
 #My Answer
 '''
